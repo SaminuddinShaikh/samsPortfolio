5db69f7a83498c7519be7edbb8ce03d510e49a34
Saminuddin Shaikh <saminuddins2@gmil.com>
2024-08-12 19:31:35 +0530

    s

---
+++ backend/middelwares/catchAsyncErrors.js
@@ -0,0 +1,7 @@
+// to promt the overlooked errors
+
+export const catchAsyncErrors = (theFunction) => {
+  return (req, res, next) => {
+    Promise.resolve(theFunction(req, res, next)).catch(next);
+  };
+};
